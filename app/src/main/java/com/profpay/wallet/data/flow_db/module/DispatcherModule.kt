9831df591132e7e7733b83b773c078829821fa49
ProfPay Ecosystem <profpayecosystem@gmail.com>
2025-10-04 02:39:32 +0300

    refactor: Move dispatcher qualifiers into DispatcherModule
    The `@IoDispatcher` and `@MainDispatcher` qualifier annotations were moved from the deleted `DispatchersQualifiers.kt` file into `DispatcherModule.kt`. This consolidation places the qualifier definitions in the same file where they are provided.

--- app/src/main/java/com/profpay/wallet/data/flow_db/module/DispatcherModule.kt
+++ app/src/main/java/com/profpay/wallet/data/flow_db/module/DispatcherModule.kt
@@ -6,6 +6,15 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
 
 @Module
 @InstallIn(SingletonComponent::class)
